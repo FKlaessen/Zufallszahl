9035bfa76e7637095f28e48de2105471711fbc74
Florian Klaessen <42743938+FKlaessen@users.noreply.github.com>
2021-02-27 10:55:14 +0100

    Add files via upload

---
+++ Zufallszahl.py
@@ -0,0 +1,36 @@
+import random, sys;
+
+
+print("Zahlenraten-Spiel: Bitte geben Sie eine Zahl zwischen 0 und 100 ein")
+zufallsZahl = random.randint(0,100)
+zahl = None
+
+while zahl != zufallsZahl:
+
+    try:
+        zahl = int(input("Ihre Zahl: "))
+        isNumber = True
+    except ValueError:
+        isNumber = False
+
+    if  isNumber :
+        if(zahl<=100 and zahl >=0):
+            if zahl > zufallsZahl: print("Ihre Zahl war größer")
+
+            elif zahl < zufallsZahl : print("Ihre Zahl war kleiner")
+        else:
+            print("Bitte geben Sie eine Zahl zwischen 0 und 100 ein")
+    else:
+        print("Es sind nur Zahlen erlaubt")
+
+
+print("Herzlichen Glückwunsch, Sie haben die Zahl erraten")
+sys.exit()  
+
+
+
+
+
+
+
+
