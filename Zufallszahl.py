96f297c4c2e32b0f06b970034dd50f61c1391b10
Florian Klaessen <42743938+FKlaessen@users.noreply.github.com>
2021-02-27 11:03:03 +0100

    Add files via upload

--- Zufallszahl.py
+++ Zufallszahl.py
@@ -9,20 +9,16 @@ while zahl != zufallsZahl:
 
     try:
         zahl = int(input("Ihre Zahl: "))
-        isNumber = True
-    except ValueError:
-        isNumber = False
-
-    if  isNumber :
         if(zahl<=100 and zahl >=0):
             if zahl > zufallsZahl: print("Ihre Zahl war größer")
 
             elif zahl < zufallsZahl : print("Ihre Zahl war kleiner")
         else:
             print("Bitte geben Sie eine Zahl zwischen 0 und 100 ein")
-    else:
-        print("Es sind nur Zahlen erlaubt")
+        
 
+    except ValueError:
+        print("Es sind nur Zahlen erlaubt")
 
 print("Herzlichen Glückwunsch, Sie haben die Zahl erraten")
 sys.exit()  
